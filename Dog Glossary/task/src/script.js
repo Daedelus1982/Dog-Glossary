94fe038613fe9fb5be23ead49d6643c6f4ee1474
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-12-30 14:12:04 +0000

    stage 2

--- Dog Glossary/task/src/script.js
+++ Dog Glossary/task/src/script.js
@@ -1,6 +1,8 @@
 const dogButton = document.getElementById("button-random-dog");
+const dogBreedButton = document.getElementById("button-show-breed");
 const dogDiv = document.getElementById("content");
 const dogImgElement = document.createElement('img');
+const breedName = document.getElementById("input-breed");
 dogImgElement.src = fetchDogPic();
 dogImgElement.alt = 'random dog image';
 const newDogDiv = document.createElement('div');
@@ -13,11 +15,31 @@ function fetchDogPic() {
     return JSON.parse(dogResponse).message;
 }
 
+function fetchByBreed(breed) {
+    const dogResponse = httpGet(`https://dog.ceo/api/breed/${breed}/images/random`);
+    return JSON.parse(dogResponse);
+}
+
 
 dogButton.addEventListener("click",() => {
+    newDogDiv.innerHTML = '';
     dogImgElement.src = fetchDogPic();
+    newDogDiv.appendChild(dogImgElement);
     dogDiv.replaceWith(newDogDiv);
-});
+})
+
+dogBreedButton.addEventListener("click", () => {
+    const response = fetchByBreed(breedName.value.toLowerCase());
+    newDogDiv.innerHTML = '';
+    if (response.status === 'error') {
+        newDogDiv.insertAdjacentHTML("beforeend", "<p>Breed not found!</p>");
+    }
+    else {
+        dogImgElement.src = response.message;
+        newDogDiv.appendChild(dogImgElement);
+    }
+    dogDiv.replaceWith(newDogDiv);
+})
 
 function httpGet(theUrl)
 {
